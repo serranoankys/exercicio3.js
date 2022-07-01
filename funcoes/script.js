b182b2796425449c19e3e9daa074785510ebe0de
“serranoankys” <serranoankys@gmail.com>
2022-07-01 15:17:03 -0300

    Primero commit

---
+++ funcoes/script.js
@@ -0,0 +1,18 @@
+//var dado = prompt("Qual e sua idade")
+//console.log
+//console.warn(dado)
+console.table(
+{
+  Nome: 'Ankys',
+  Sobrenome: 'Serrano',
+  Idade: '40',
+}
+)
+
+console.table(
+{
+  Nome: 'Ruvi',
+  Sobrenome: 'Piter',
+  Idade: '07',
+}
+)
